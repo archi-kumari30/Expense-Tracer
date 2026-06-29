04c545fc97d454aece1e56c82db55fde906566ca
Archi Kumari <kumariarchi926@gmail.com>
2026-06-29 08:01:31 +0530

    first-commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let text=document.getElementById("text");
+let number=document.getElementById("number");
+let transaction=document.getElementById("transaction");
+let print=document.getElementById("print");
 
+
+transaction.addEventListener("click",()=>{
+ let textValue=text.value;
+ let numberValue=number.value;
+
+ print.innerHTML+="<p>"+textValue+"  "+numberValue+"</p>";
+})
